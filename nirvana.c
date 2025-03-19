99d498e7a1cbfaf513ebb332203d9c4921d9adfd
yuriyuri824 <yuritarkov1024@gmail.com>
2025-03-19 11:26:25 +0700

    first commit

---
+++ nirvana.c
@@ -0,0 +1,22 @@
+#define NIRVANA_IMPL
+#define NIRVANA_ENABLE_LOG
+#include "nirvana.h"
+
+int main(void) {
+    nir_cmd_goes_samsara();
+
+    nir_cmd_t cmd = {0};
+    if (!nir_cmd_init(&cmd)) {
+        fprintf(stderr, "Couldn't initialize CMD\n");
+        return 1;
+    }
+    nir_cmd_append(&cmd, "gcc");
+    nir_cmd_append(&cmd, "-o");
+    nir_cmd_append(&cmd, "test");
+    nir_cmd_append(&cmd, "test.c");
+    nir_cmd_append(&cmd, "-Wall -Werror -Wextra -pedantic");
+    
+    nir_cmd_run(&cmd);
+    nir_cmd_cleanup(&cmd);
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Built with nirvana.h!\n");
+    return 0;
+}
